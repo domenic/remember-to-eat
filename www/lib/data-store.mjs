domenic/remember-to-eat

commit 9f92346042d3bda562659fd01060a1b7e782ea7f

Change "sleep" time to "last meal" time

www/lib/data-store.mjs
@@ -17,7 +17,13 @@ export class TargetsStore {
 
   async get() {
     await this.#ensureDBOpened();
-    return this.#db.get(TargetsStore.#objectStore, TargetsStore.#key);
+    const targets = await this.#db.get(TargetsStore.#objectStore, TargetsStore.#key);
+
+    // Data migration
+    if (targets && 'sleepTime' in targets) {
+      targets.lastMealTime = targets.sleepTime;
+      delete targets.sleepTime;
+    }
   }
 
   async clear() {

__tests__/time.mjs
@@ -8,11 +8,11 @@ afterAll(() => {
 });
 
 describe('progress()', () => {
-  describe('sleep before midnight', () => {
+  describe('last meal before midnight', () => {
     runTest({
       description: 'now before wakeUp',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T23:00Z',
+      lastmeal: '1970-01-01T23:00Z',
       now: '2020-05-22T07:00',
       expected: 1
     });
@@ -20,41 +20,41 @@ describe('progress()', () => {
     runTest({
       description: 'now = wakeUp',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T23:00Z',
+      lastmeal: '1970-01-01T23:00Z',
       now: '2020-05-22T09:00',
       expected: 0
     });
 
     runTest({
-      description: 'now between wakeUp and sleep',
+      description: 'now between wakeUp and lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T23:00Z',
+      lastmeal: '1970-01-01T23:00Z',
       now: '2020-05-22T11:00',
       expected: 2 / 14
     });
 
     runTest({
-      description: 'now = sleep',
+      description: 'now = lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T23:00Z',
+      lastmeal: '1970-01-01T23:00Z',
       now: '2020-05-22T23:00',
       expected: 1
     });
 
     runTest({
-      description: 'now after sleep',
+      description: 'now after lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T23:00Z',
+      lastmeal: '1970-01-01T23:00Z',
       now: '2020-05-22T23:30',
       expected: 1
     });
   });
 
-  describe('sleep after midnight', () => {
+  describe('last meal after midnight', () => {
     runTest({
       description: 'now before wakeup',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T01:00Z',
+      lastmeal: '1970-01-01T01:00Z',
       now: '2020-05-22T07:00',
       expected: 1
     });
@@ -62,53 +62,53 @@ describe('progress()', () => {
     runTest({
       description: 'now = wakeUp',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T01:00Z',
+      lastmeal: '1970-01-01T01:00Z',
       now: '2020-05-22T09:00',
       expected: 0
     });
 
     runTest({
-      description: 'now between wakeUp and sleep',
+      description: 'now between wakeUp and lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T01:00Z',
+      lastmeal: '1970-01-01T01:00Z',
       now: '2020-05-22T11:00',
       expected: 2 / 16
     });
 
     runTest({
-      description: 'now = sleep',
+      description: 'now = lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T01:00Z',
+      lastmeal: '1970-01-01T01:00Z',
       now: '2020-05-22T01:00',
       expected: 1
     });
 
     runTest({
-      description: 'now after sleep',
+      description: 'now after lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T01:00Z',
+      lastmeal: '1970-01-01T01:00Z',
       now: '2020-05-22T01:30',
       expected: 1
     });
   });
 
-  function runTest({ description, wakeUp, sleep, now, expected }) {
+  function runTest({ description, wakeUp, lastMeal, now, expected }) {
     it(description, () => {
       const wakeUpValueAsNumber = (new Date(wakeUp)).valueOf();
-      const sleepValueAsNumber = (new Date(sleep)).valueOf();
+      const lastMealValueAsNumber = (new Date(lastMeal)).valueOf();
       clock.setSystemTime(new Date(now));
 
-      expect(progress(wakeUpValueAsNumber, sleepValueAsNumber)).toBe(expected);
+      expect(progress(wakeUpValueAsNumber, lastMealValueAsNumber)).toBe(expected);
     });
   }
 });
 
 describe('boundsOfToday()', () => {
-  describe('sleep before midnight', () => {
+  describe('last meal before midnight', () => {
     runTest({
       description: 'now before first midpoint',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T23:00Z',
+      lastmeal: '1970-01-01T23:00Z',
       now: '2020-05-22T03:00',
       expected: ['2020-05-21T04:00', '2020-05-22T04:00']
     });
@@ -116,7 +116,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now before wakeUp',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T23:00Z',
+      lastmeal: '1970-01-01T23:00Z',
       now: '2020-05-22T07:00',
       expected: ['2020-05-22T04:00', '2020-05-23T04:00']
     });
@@ -124,41 +124,41 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now = wakeUp',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T23:00Z',
+      lastmeal: '1970-01-01T23:00Z',
       now: '2020-05-22T09:00',
       expected: ['2020-05-22T04:00', '2020-05-23T04:00']
     });
 
     runTest({
-      description: 'now between wakeUp and sleep',
+      description: 'now between wakeUp and lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T23:00Z',
+      lastmeal: '1970-01-01T23:00Z',
       now: '2020-05-22T11:00',
       expected: ['2020-05-22T04:00', '2020-05-23T04:00']
     });
 
     runTest({
-      description: 'now = sleep',
+      description: 'now = lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T23:00Z',
+      lastmeal: '1970-01-01T23:00Z',
       now: '2020-05-22T23:00',
       expected: ['2020-05-22T04:00', '2020-05-23T04:00']
     });
 
     runTest({
-      description: 'now after sleep',
+      description: 'now after lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T23:00Z',
+      lastmeal: '1970-01-01T23:00Z',
       now: '2020-05-22T23:30',
       expected: ['2020-05-22T04:00', '2020-05-23T04:00']
     });
   });
 
-  describe('sleep at midnight', () => {
+  describe('last meal at midnight', () => {
     runTest({
       description: 'now before first midpoint',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T00:00Z',
+      lastmeal: '1970-01-01T00:00Z',
       now: '2020-05-22T03:00',
       expected: ['2020-05-21T04:30', '2020-05-22T04:30']
     });
@@ -166,7 +166,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now before wakeup',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T00:00Z',
+      lastmeal: '1970-01-01T00:00Z',
       now: '2020-05-22T07:00',
       expected: ['2020-05-22T04:30', '2020-05-23T04:30']
     });
@@ -174,41 +174,41 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now = wakeUp',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T00:00Z',
+      lastmeal: '1970-01-01T00:00Z',
       now: '2020-05-22T09:00',
       expected: ['2020-05-22T04:30', '2020-05-23T04:30']
     });
 
     runTest({
-      description: 'now between wakeUp and sleep',
+      description: 'now between wakeUp and lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T00:00Z',
+      lastmeal: '1970-01-01T00:00Z',
       now: '2020-05-22T11:00',
       expected: ['2020-05-22T04:30', '2020-05-23T04:30']
     });
 
     runTest({
-      description: 'now = sleep',
+      description: 'now = lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T00:00Z',
+      lastmeal: '1970-01-01T00:00Z',
       now: '2020-05-23T00:00',
       expected: ['2020-05-22T04:30', '2020-05-23T04:30']
     });
 
     runTest({
-      description: 'now after sleep',
+      description: 'now after lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T00:00Z',
+      lastmeal: '1970-01-01T00:00Z',
       now: '2020-05-23T01:30',
       expected: ['2020-05-22T04:30', '2020-05-23T04:30']
     });
   });
 
-  describe('sleep after midnight', () => {
+  describe('last meal after midnight', () => {
     runTest({
       description: 'now before first midpoint',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T01:00Z',
+      lastmeal: '1970-01-01T01:00Z',
       now: '2020-05-22T03:00',
       expected: ['2020-05-21T05:00', '2020-05-22T05:00']
     });
@@ -216,7 +216,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now before wakeup',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T01:00Z',
+      lastmeal: '1970-01-01T01:00Z',
       now: '2020-05-22T07:00',
       expected: ['2020-05-22T05:00', '2020-05-23T05:00']
     });
@@ -224,45 +224,45 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now = wakeUp',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T01:00Z',
+      lastmeal: '1970-01-01T01:00Z',
       now: '2020-05-22T09:00',
       expected: ['2020-05-22T05:00', '2020-05-23T05:00']
     });
 
     runTest({
-      description: 'now between wakeUp and sleep',
+      description: 'now between wakeUp and lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T01:00Z',
+      lastmeal: '1970-01-01T01:00Z',
       now: '2020-05-22T11:00',
       expected: ['2020-05-22T05:00', '2020-05-23T05:00']
     });
 
     runTest({
-      description: 'now = sleep',
+      description: 'now = lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T01:00Z',
+      lastmeal: '1970-01-01T01:00Z',
       now: '2020-05-23T01:00',
       expected: ['2020-05-22T05:00', '2020-05-23T05:00']
     });
 
     runTest({
-      description: 'now after sleep',
+      description: 'now after lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      sleep: '1970-01-01T01:00Z',
+      lastmeal: '1970-01-01T01:00Z',
       now: '2020-05-23T01:30',
       expected: ['2020-05-22T05:00', '2020-05-23T05:00']
     });
   });
 
 
-  function runTest({ description, wakeUp, sleep, now, expected }) {
+  function runTest({ description, wakeUp, lastMeal, now, expected }) {
     it(description, () => {
       const wakeUpValueAsNumber = (new Date(wakeUp)).valueOf();
-      const sleepValueAsNumber = (new Date(sleep)).valueOf();
+      const lastMealValueAsNumber = (new Date(lastMeal)).valueOf();
       clock.setSystemTime(new Date(now));
 
       const expectedAsDates = expected.map(e => new Date(e));
-      expect(boundsOfToday(wakeUpValueAsNumber, sleepValueAsNumber)).toEqual(expectedAsDates);
+      expect(boundsOfToday(wakeUpValueAsNumber, lastMealValueAsNumber)).toEqual(expectedAsDates);
     });
   }
 });

www/lib/time.mjs
@@ -1,25 +1,25 @@
 const ONE_DAY = 24 * 60 * 60 * 1000;
 
-export function progress(wakeUpTimeValueAsNumber, sleepTimeValueAsNumber) {
-  const offsets = getTimeOffsets(wakeUpTimeValueAsNumber, sleepTimeValueAsNumber);
+export function progress(wakeUpTimeValueAsNumber, lastMealTimeValueAsNumber) {
+  const offsets = getTimeOffsets(wakeUpTimeValueAsNumber, lastMealTimeValueAsNumber);
   if (!offsets) {
     return null;
   }
 
   const nowOffset = getNowOffset(offsets.wakeUp);
 
-  return Math.min(1, (nowOffset - offsets.wakeUp) / (offsets.sleep - offsets.wakeUp));
+  return Math.min(1, (nowOffset - offsets.wakeUp) / (offsets.lastMeal - offsets.wakeUp));
 }
 
-// Returns a 24-hour period centered around "today" from one sleep/wakeup midpoint to the next.
-export function boundsOfToday(wakeUpTimeValueAsNumber, sleepTimeValueAsNumber) {
-  const offsets = getTimeOffsets(wakeUpTimeValueAsNumber, sleepTimeValueAsNumber);
+// Returns a 24-hour period centered around "today" from one lastMeal/wakeup midpoint to the next.
+export function boundsOfToday(wakeUpTimeValueAsNumber, lastMealTimeValueAsNumber) {
+  const offsets = getTimeOffsets(wakeUpTimeValueAsNumber, lastMealTimeValueAsNumber);
   if (!offsets) {
     return null;
   }
 
-  const msBetweenSleepAndWakeUp = positiveMod(offsets.wakeUp - offsets.sleep, ONE_DAY);
-  const midpointOffset = positiveMod(offsets.sleep + msBetweenSleepAndWakeUp / 2, ONE_DAY);
+  const msBetweenLastMealAndWakeUp = positiveMod(offsets.wakeUp - offsets.lastMeal, ONE_DAY);
+  const midpointOffset = positiveMod(offsets.lastMeal + msBetweenLastMealAndWakeUp / 2, ONE_DAY);
 
   const midnightDate = new Date();
   midnightDate.setHours(0, 0, 0, 0);
@@ -34,24 +34,24 @@ export function boundsOfToday(wakeUpTimeValueAsNumber, sleepTimeValueAsNumber) {
   return [midpoint, nextMidpoint];
 }
 
-// Returns the number of milliseconds past midnight for the wake-up time and sleep time.
-function getTimeOffsets(wakeUpTimeValueAsNumber, sleepTimeValueAsNumber) {
+// Returns the number of milliseconds past midnight for the wake-up time and lastMeal time.
+function getTimeOffsets(wakeUpTimeValueAsNumber, lastMealTimeValueAsNumber) {
   // Inputs are not in a valid state
-  if (wakeUpTimeValueAsNumber === null || sleepTimeValueAsNumber === null) {
+  if (wakeUpTimeValueAsNumber === null || lastMealTimeValueAsNumber === null) {
     return null;
   }
 
-  const adjustedSleepTimeValueAsNumber = sleepTimeValueAsNumber < wakeUpTimeValueAsNumber ?
-    sleepTimeValueAsNumber + ONE_DAY :
-    sleepTimeValueAsNumber;
+  const adjustedLastMealTimeValueAsNumber = lastMealTimeValueAsNumber < wakeUpTimeValueAsNumber ?
+    lastMealTimeValueAsNumber + ONE_DAY :
+    lastMealTimeValueAsNumber;
 
-  // Right now wakeUpDate and sleepDate are numbers representing milliseconds since midnight UTC
+  // Right now wakeUpDate and lastMealDate are numbers representing milliseconds since midnight UTC
   // on 1970-01-01, since valueAsDate uses 1970-01-01T:00:00:00Z as its base. We want to translate
-  // them to be milliseconds-since-midnight (potentially greater than 24 hours if the sleep time
-  // is less than the wake-up time, e.g. wake up at 09:00 sleep at 00:30).
+  // them to be milliseconds-since-midnight (potentially greater than 24 hours if the lastMeal time
+  // is less than the wake-up time, e.g. wake up at 09:00 lastMeal at 00:30).
 
   const epoch = (new Date(0)).valueOf();
-  return { wakeUp: wakeUpTimeValueAsNumber - epoch, sleep: adjustedSleepTimeValueAsNumber - epoch };
+  return { wakeUp: wakeUpTimeValueAsNumber - epoch, lastMeal: adjustedLastMealTimeValueAsNumber - epoch };
 }
 
 // Returns the number of milliseconds past midnight for the current time, assuming that if the

__tests__/targets.mjs
@@ -6,15 +6,15 @@ describe('Targets class', () => {
       <input type="number" id="energy" value="5">
       <input type="number" id="protein" value="10">
       <input type="time" id="wake-up" value="10:00:00">
-      <input type="time" id="sleep" value="00:30:00">
+      <input type="time" id="last-meal" value="00:30:00">
     `;
 
-    const targets = new Targets('#energy', '#protein', '#wake-up', '#sleep');
+    const targets = new Targets('#energy', '#protein', '#wake-up', '#last-meal');
 
     expect(targets.energy).toEqual(5);
     expect(targets.protein).toEqual(10);
     expect(targets.wakeUpTime).toEqual((new Date('1970-01-01T10:00:00Z')).valueOf());
-    expect(targets.sleepTime).toEqual((new Date('1970-01-01T00:30:00Z')).valueOf());
+    expect(targets.lastMealTime).toEqual((new Date('1970-01-01T00:30:00Z')).valueOf());
   });
 
   it('should return the serialization of the HTML values from serialization()', () => {
@@ -22,16 +22,16 @@ describe('Targets class', () => {
       <input type="number" id="energy" value="5">
       <input type="number" id="protein" value="10">
       <input type="time" id="wake-up" value="10:00:00">
-      <input type="time" id="sleep" value="00:30:00">
+      <input type="time" id="last-meal" value="00:30:00">
     `;
 
-    const targets = new Targets('#energy', '#protein', '#wake-up', '#sleep');
+    const targets = new Targets('#energy', '#protein', '#wake-up', '#last-meal');
 
     expect(targets.serialization()).toEqual({
       energy: 5,
       protein: 10,
       wakeUpTime: (new Date('1970-01-01T10:00:00Z')).valueOf(),
-      sleepTime: (new Date('1970-01-01T00:30:00Z')).valueOf()
+      lastMealTime: (new Date('1970-01-01T00:30:00Z')).valueOf()
     });
   });
 
@@ -40,22 +40,22 @@ describe('Targets class', () => {
       <input type="number" id="energy" value="5">
       <input type="number" id="protein" value="10">
       <input type="time" id="wake-up" value="10:00:00">
-      <input type="time" id="sleep" value="00:30:00">
+      <input type="time" id="last-meal" value="00:30:00">
     `;
 
     const newEnergy = 15;
     const newProtein = 100;
     const newWakeUpTime = (new Date('1970-01-01T11:00:00Z')).valueOf();
-    const newSleepTime = (new Date('1970-01-01T23:00:00Z')).valueOf();
+    const newLastMealTime = (new Date('1970-01-01T23:00:00Z')).valueOf();
     const newSerialization = {
       energy: newEnergy,
       protein: newProtein,
       wakeUpTime: newWakeUpTime,
-      sleepTime: newSleepTime
+      lastMealTime: newLastMealTime
     };
 
     let changes = 0;
-    const targets = new Targets('#energy', '#protein', '#wake-up', '#sleep');
+    const targets = new Targets('#energy', '#protein', '#wake-up', '#last-meal');
     targets.addEventListener('change', () => {
       ++changes;
     });
@@ -65,14 +65,14 @@ describe('Targets class', () => {
     expect(targets.energy).toEqual(newEnergy);
     expect(targets.protein).toEqual(newProtein);
     expect(targets.wakeUpTime).toEqual(newWakeUpTime);
-    expect(targets.sleepTime).toEqual(newSleepTime);
+    expect(targets.lastMealTime).toEqual(newLastMealTime);
     expect(targets.serialization()).toEqual(newSerialization);
     expect(changes).toEqual(1);
 
     expect(document.body.querySelector('#energy').valueAsNumber).toEqual(newEnergy);
     expect(document.body.querySelector('#protein').valueAsNumber).toEqual(newProtein);
     expect(document.body.querySelector('#wake-up').valueAsNumber).toEqual(newWakeUpTime);
-    expect(document.body.querySelector('#sleep').valueAsNumber).toEqual(newSleepTime);
+    expect(document.body.querySelector('#last-meal').valueAsNumber).toEqual(newLastMealTime);
   });
 
   it('should fire a change event if any input events happen', () => {
@@ -80,11 +80,11 @@ describe('Targets class', () => {
       <input type="number" id="energy" value="5">
       <input type="number" id="protein" value="10">
       <input type="time" id="wake-up" value="10:00:00">
-      <input type="time" id="sleep" value="00:30:00">
+      <input type="time" id="last-meal" value="00:30:00">
     `;
 
     let changes = 0;
-    const targets = new Targets('#energy', '#protein', '#wake-up', '#sleep');
+    const targets = new Targets('#energy', '#protein', '#wake-up', '#last-meal');
     targets.addEventListener('change', () => {
       ++changes;
     });
@@ -98,7 +98,7 @@ describe('Targets class', () => {
     document.querySelector('#wake-up').dispatchEvent(new Event('input'));
     expect(changes).toEqual(3);
 
-    document.querySelector('#sleep').dispatchEvent(new Event('input'));
+    document.querySelector('#last-meal').dispatchEvent(new Event('input'));
     expect(changes).toEqual(4);
   });
 });

www/index.html
@@ -56,8 +56,8 @@ navigator.serviceWorker.register('/service-worker.js');
         </label>
 
         <label class="time">
-          Sleep
-          <input type="time" id="sleep-time" value="00:30">
+          Last meal
+          <input type="time" id="last-meal-time" value="00:30">
         </label>
       </div>
     </div>

commit 5ea7a7c04c24dc3e3472ff8367c7f11187dae3e7

Fix recently-introduced bugs
9f92346042d3bda562659fd01060a1b7e782ea7f broke target restoration from the data store, and broke the tests for the time module.

It would be good to actually run tests tests in CI!

www/lib/data-store.mjs
@@ -24,6 +24,8 @@ export class TargetsStore {
       targets.lastMealTime = targets.sleepTime;
       delete targets.sleepTime;
     }
+
+    return targets;
   }
 
   async clear() {

__tests__/time.mjs
@@ -12,7 +12,7 @@ describe('progress()', () => {
     runTest({
       description: 'now before wakeUp',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T23:00Z',
+      lastMeal: '1970-01-01T23:00Z',
       now: '2020-05-22T07:00',
       expected: 1
     });
@@ -20,7 +20,7 @@ describe('progress()', () => {
     runTest({
       description: 'now = wakeUp',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T23:00Z',
+      lastMeal: '1970-01-01T23:00Z',
       now: '2020-05-22T09:00',
       expected: 0
     });
@@ -28,7 +28,7 @@ describe('progress()', () => {
     runTest({
       description: 'now between wakeUp and lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T23:00Z',
+      lastMeal: '1970-01-01T23:00Z',
       now: '2020-05-22T11:00',
       expected: 2 / 14
     });
@@ -36,7 +36,7 @@ describe('progress()', () => {
     runTest({
       description: 'now = lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T23:00Z',
+      lastMeal: '1970-01-01T23:00Z',
       now: '2020-05-22T23:00',
       expected: 1
     });
@@ -44,7 +44,7 @@ describe('progress()', () => {
     runTest({
       description: 'now after lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T23:00Z',
+      lastMeal: '1970-01-01T23:00Z',
       now: '2020-05-22T23:30',
       expected: 1
     });
@@ -54,7 +54,7 @@ describe('progress()', () => {
     runTest({
       description: 'now before wakeup',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T01:00Z',
+      lastMeal: '1970-01-01T01:00Z',
       now: '2020-05-22T07:00',
       expected: 1
     });
@@ -62,7 +62,7 @@ describe('progress()', () => {
     runTest({
       description: 'now = wakeUp',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T01:00Z',
+      lastMeal: '1970-01-01T01:00Z',
       now: '2020-05-22T09:00',
       expected: 0
     });
@@ -70,7 +70,7 @@ describe('progress()', () => {
     runTest({
       description: 'now between wakeUp and lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T01:00Z',
+      lastMeal: '1970-01-01T01:00Z',
       now: '2020-05-22T11:00',
       expected: 2 / 16
     });
@@ -78,7 +78,7 @@ describe('progress()', () => {
     runTest({
       description: 'now = lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T01:00Z',
+      lastMeal: '1970-01-01T01:00Z',
       now: '2020-05-22T01:00',
       expected: 1
     });
@@ -86,7 +86,7 @@ describe('progress()', () => {
     runTest({
       description: 'now after lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T01:00Z',
+      lastMeal: '1970-01-01T01:00Z',
       now: '2020-05-22T01:30',
       expected: 1
     });
@@ -108,7 +108,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now before first midpoint',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T23:00Z',
+      lastMeal: '1970-01-01T23:00Z',
       now: '2020-05-22T03:00',
       expected: ['2020-05-21T04:00', '2020-05-22T04:00']
     });
@@ -116,7 +116,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now before wakeUp',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T23:00Z',
+      lastMeal: '1970-01-01T23:00Z',
       now: '2020-05-22T07:00',
       expected: ['2020-05-22T04:00', '2020-05-23T04:00']
     });
@@ -124,7 +124,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now = wakeUp',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T23:00Z',
+      lastMeal: '1970-01-01T23:00Z',
       now: '2020-05-22T09:00',
       expected: ['2020-05-22T04:00', '2020-05-23T04:00']
     });
@@ -132,7 +132,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now between wakeUp and lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T23:00Z',
+      lastMeal: '1970-01-01T23:00Z',
       now: '2020-05-22T11:00',
       expected: ['2020-05-22T04:00', '2020-05-23T04:00']
     });
@@ -140,7 +140,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now = lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T23:00Z',
+      lastMeal: '1970-01-01T23:00Z',
       now: '2020-05-22T23:00',
       expected: ['2020-05-22T04:00', '2020-05-23T04:00']
     });
@@ -148,7 +148,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now after lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T23:00Z',
+      lastMeal: '1970-01-01T23:00Z',
       now: '2020-05-22T23:30',
       expected: ['2020-05-22T04:00', '2020-05-23T04:00']
     });
@@ -158,7 +158,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now before first midpoint',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T00:00Z',
+      lastMeal: '1970-01-01T00:00Z',
       now: '2020-05-22T03:00',
       expected: ['2020-05-21T04:30', '2020-05-22T04:30']
     });
@@ -166,7 +166,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now before wakeup',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T00:00Z',
+      lastMeal: '1970-01-01T00:00Z',
       now: '2020-05-22T07:00',
       expected: ['2020-05-22T04:30', '2020-05-23T04:30']
     });
@@ -174,7 +174,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now = wakeUp',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T00:00Z',
+      lastMeal: '1970-01-01T00:00Z',
       now: '2020-05-22T09:00',
       expected: ['2020-05-22T04:30', '2020-05-23T04:30']
     });
@@ -182,7 +182,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now between wakeUp and lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T00:00Z',
+      lastMeal: '1970-01-01T00:00Z',
       now: '2020-05-22T11:00',
       expected: ['2020-05-22T04:30', '2020-05-23T04:30']
     });
@@ -190,7 +190,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now = lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T00:00Z',
+      lastMeal: '1970-01-01T00:00Z',
       now: '2020-05-23T00:00',
       expected: ['2020-05-22T04:30', '2020-05-23T04:30']
     });
@@ -198,7 +198,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now after lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T00:00Z',
+      lastMeal: '1970-01-01T00:00Z',
       now: '2020-05-23T01:30',
       expected: ['2020-05-22T04:30', '2020-05-23T04:30']
     });
@@ -208,7 +208,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now before first midpoint',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T01:00Z',
+      lastMeal: '1970-01-01T01:00Z',
       now: '2020-05-22T03:00',
       expected: ['2020-05-21T05:00', '2020-05-22T05:00']
     });
@@ -216,7 +216,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now before wakeup',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T01:00Z',
+      lastMeal: '1970-01-01T01:00Z',
       now: '2020-05-22T07:00',
       expected: ['2020-05-22T05:00', '2020-05-23T05:00']
     });
@@ -224,7 +224,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now = wakeUp',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T01:00Z',
+      lastMeal: '1970-01-01T01:00Z',
       now: '2020-05-22T09:00',
       expected: ['2020-05-22T05:00', '2020-05-23T05:00']
     });
@@ -232,7 +232,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now between wakeUp and lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T01:00Z',
+      lastMeal: '1970-01-01T01:00Z',
       now: '2020-05-22T11:00',
       expected: ['2020-05-22T05:00', '2020-05-23T05:00']
     });
@@ -240,7 +240,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now = lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T01:00Z',
+      lastMeal: '1970-01-01T01:00Z',
       now: '2020-05-23T01:00',
       expected: ['2020-05-22T05:00', '2020-05-23T05:00']
     });
@@ -248,7 +248,7 @@ describe('boundsOfToday()', () => {
     runTest({
       description: 'now after lastMeal',
       wakeUp: '1970-01-01T09:00Z',
-      lastmeal: '1970-01-01T01:00Z',
+      lastMeal: '1970-01-01T01:00Z',
       now: '2020-05-23T01:30',
       expected: ['2020-05-22T05:00', '2020-05-23T05:00']
     });